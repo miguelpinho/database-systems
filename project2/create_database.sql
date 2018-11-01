92a3f2230e2cc45744ee704e29bcbdee9052704a
miguelp <33704675+miguelpinho@users.noreply.github.com>
2018-11-01 22:43:45 +0000

    start create db, project 2 (carlitos)

---
+++ project2/create_database.sql
@@ -0,0 +1,84 @@
+create table person
+	(VAT integer,
+	 name varchar(255),
+	 address_street varchar(255),
+	 address_city varchar(255),
+	 address_zip varchar(255),
+	 primary Key(VAT));
+
+create table phone_number
+	(VAT integer,
+	 phone integer,
+	 primary key(VAT),
+	 foreign key(VAT) references person(VAT));
+
+create table client
+	(VAT integer,
+	 primary key(VAT),
+	 foreign key(VAT) references person(VAT));
+
+create table veterinary
+	(VAT integer,
+	 specialization varchar(255),
+	 bio varchar(255),
+	 primary key(VAT),
+	 foreign key(VAT) references person(VAT));
+
+create table assistant
+	(VAT integer,
+	 primary key(VAT),
+	 foreign key(VAT) references person(VAT));
+
+create table species
+	(name varchar(255),
+	 desc varchar(255),
+	 primary key(name));
+
+create table generalization_species
+	(name1 varchar(255),
+	 name2 varchar(255),
+	 primary key(name1),
+	 foreign key(name1) references species(name1),
+	 foreign key(name2) references species(name2));
+	 
+create table animal
+	(name varchar(255),
+	 VAT integer,
+	 species_name varchar(255),
+	 colour varchar(255),
+	 gender varchar(255),
+	 birth_year date);
+/*Falta acabar o animal, não sei como se faz  A  idade xD*/
+
+create table consult
+	(name, varchar(255),
+	 VAT_owner integer,
+	 date_timestamp timestamp,
+	 s varchar(255),
+	 o varchar(255),
+	 a varchar(255),
+	 p varchar(255),
+	 VAT_client integer,
+	 VAT_vet integer,
+	 weight int unsigned not NULL, /*COnfimei se é assim por favor!!!*/
+	 primary key( name, VAT_owner, date_timestamp),
+	 foreign key( name) references animal(name),
+	 foreign key(VAT_owner) references animal (VAT),
+	 foreign key(VAT_client) references client(VAT),
+	 foreign key( VAT_vet) references veterinary(VAT));
+
+create table participation
+	(name varchar(255),
+	 VAT_owner integer,
+	 date_timestamp timestamp,
+	 VAT_assistant integer,
+	 primary key(name, VAT_owner, date_timestamp, VAT_assistant),
+	 foreign key(name) references consult(name),
+	 foreign key(VAT_owner) references consult(VAT_owner),
+	 foreign key(date_timestamp) references consult(date_timestamp),
+	 foreign key(VAT_assistant) references assistant(VAT));
+
+create table diagnosis_code
+	(code varchar(255),
+	 name varchar(255),
+	 primary key(code));
