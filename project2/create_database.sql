87bbcba748df3eb4e40087d36a8401d327344f6d
miguelp <33704675+miguelpinho@users.noreply.github.com>
2018-11-02 11:45:30 +0000

    refactored

--- project2/create_database.sql
+++ project2/create_database.sql
@@ -1,85 +1,83 @@
-create table person
-	(VAT integer,
-	 name varchar(35),
-	 address_street varchar(35),
-	 address_city varchar(15),
-	 address_zip varchar(15),
-	 primary Key(VAT));
+CREATE TABLE person
+	(VAT INTEGER,
+	 name VARCHAR(35),
+	 address_street VARCHAR(35),
+	 address_city VARCHAR(15),
+	 address_zip VARCHAR(15),
+	 PRIMARY KEY(VAT));
 
-create table phone_number
+CREATE TABLE phone_number
 	(VAT integer,
 	 phone integer,
-	 primary key(VAT, phone),
-	 foreign key(VAT) references person(VAT));
+	 PRIMARY KEY(VAT, phone),
+	 FOREIGN KEY(VAT) REFERENCES person(VAT));
 
-create table client
-	(VAT integer,
-	 primary key(VAT),
-	 foreign key(VAT) references person(VAT));
+CREATE TABLE client
+	(VAT INTEGER,
+	 PRIMARY KEY(VAT),
+	 FOREIGN KEY(VAT) REFERENCES person(VAT));
 
-create table veterinary
-	(VAT integer,
-	 specialization varchar(20),
-	 bio varchar(255),
-	 primary key(VAT),
-	 foreign key(VAT) references person(VAT));
+CREATE TABLE veterinary
+	(VAT INTEGER,
+	 specialization VARCHAR(20),
+	 bio VARCHAR(255),
+	 PRIMARY KEY(VAT),
+	 FOREIGN KEY(VAT) REFERENCES person(VAT));
 
-create table assistant
-	(VAT integer,
-	 primary key(VAT),
-	 foreign key(VAT) references person(VAT));
+CREATE TABLE assistant
+	(VAT INTEGER,
+	 PRIMARY KEY(VAT),
+	 FOREIGN KEY(VAT) REFERENCES person(VAT));
 
-create table species
-	(name varchar(35),
-	 descript varchar(100),
-	 primary key(name));
+CREATE TABLE species
+	(name VARCHAR(35),
+	 descript VARCHAR(100),
+	 PRIMARY KEY(name));
 
-create table generalization_species
-	(name1 varchar(35),
-	 name2 varchar(35),
-	 primary key(name1),
-	 foreign key(name1) references species(name),
-	 foreign key(name2) references species(name));
+CREATE TABLE generalization_species
+	(name1 VARCHAR(35),
+	 name2 VARCHAR(35),
+	 PRIMARY KEY(name1),
+	 FOREIGN KEY(name1) REFERENCES species(name),
+	 FOREIGN KEY(name2) REFERENCES species(name));
 	 
-create table animal
-	(name varchar(35),
-	 VAT integer,
-	 species_name varchar(35),
-	 colour varchar(15),
-	 gender varchar(15),
-	 birth_year date);
+CREATE TABLE animal
+	(name VARCHAR(35),
+	 VAT INTEGER,
+	 species_name VARCHAR(35),
+	 colour VARCHAR(15),
+	 gender VARCHAR(15),
+	 birth_year DATE);
 /*Falta acabar o animal, não sei como se faz  A  idade xD*/
 
-create table consult
-	(name varchar(35),
-	 VAT_owner integer,
+CREATE TABLE consult
+	(name VARCHAR(35),
+	 VAT_owner INTEGER,
 	 date_timestamp timestamp,
-	 s varchar(255),
-	 o varchar(255),
-	 a varchar(255),
-	 p varchar(255),
-	 VAT_client integer,
-	 VAT_vet integer,
-	 weight integer not NULL, /*COnfimei se é assim por favor!!!*/
-	 primary key(name, VAT_owner, date_timestamp),
-	 foreign key(name) references animal(name),
-	 foreign key(VAT_owner) references animal (VAT),
-	 foreign key(VAT_client) references client(VAT),
-	 foreign key( VAT_vet) references veterinary(VAT)
-     check (weight >= 0));
+	 s VARCHAR(255),
+	 o VARCHAR(255),
+	 a VARCHAR(255),
+	 p VARCHAR(255),
+	 VAT_client INTEGER,
+	 VAT_vet INTEGER,
+	 weight INTEGER NOT NULL, /*COnfimei se é assim por favor!!!*/
+	 PRIMARY KEY(name, VAT_owner, date_timestamp),
+	 FOREIGN KEY(name) REFERENCES animal(name),
+	 FOREIGN KEY(VAT_owner) REFERENCES animal(VAT),
+	 FOREIGN KEY(VAT_client) REFERENCES client(VAT),
+	 FOREIGN KEY(VAT_vet) REFERENCES veterinary(VAT),
+     CHECK (weight > 0));
 
-create table participation
-	(name varchar(255),
-	 VAT_owner integer,
-	 date_timestamp timestamp,
-	 VAT_assistant integer,
-	 primary key(name, VAT_owner, date_timestamp, VAT_assistant),
-	 foreign key(name) references consult(name),
-	 foreign key(VAT_owner) references consult(VAT_owner),
-	 foreign key(date_timestamp) references consult(date_timestamp),
-	 foreign key(VAT_assistant) references assistant(VAT));
+CREATE TABLE participation
+	(name VARCHAR(35),
+	 VAT_owner INTEGER,
+	 date_timestamp TIMESTAMP,
+	 VAT_assistant INTEGER,
+	 PRIMARY KEY(name, VAT_owner, date_timestamp, VAT_assistant),
+	 FOREIGN KEY(name, VAT_owner, date_timestamp) REFERENCES consult(name, VAT_owner, date_timestamp),
+	 FOREIGN KEY(VAT_assistant) references assistant(VAT));
 
-create table diagnosis_code
-	(code varchar(255),
-	 name varchar(255),
-	 primary key(code));
+CREATE TABLE diagnosis_code
+	(code VARCHAR(255),
+	 name VARCHAR(255),
+	 PRIMARY KEY(code));
