395b5d2a877f89a907e6cf2c01f42543e81b2cad
miguelp <33704675+miguelpinho@users.noreply.github.com>
2018-11-03 16:03:24 +0000

    some database populate implemented

--- project2/create_database.sql
+++ project2/create_database.sql
@@ -1,191 +1,191 @@
 CREATE TABLE person
-	(VAT INTEGER,
-	 name VARCHAR(35),
-	 address_street VARCHAR(35),
-	 address_city VARCHAR(15),
-	 address_zip VARCHAR(15),
-	 PRIMARY KEY(VAT));
+    (VAT INTEGER,
+     name VARCHAR(35),
+     address_street VARCHAR(35),
+     address_city VARCHAR(15),
+     address_zip VARCHAR(15),
+     PRIMARY KEY(VAT));
 
 CREATE TABLE phone_number
-	(VAT integer,
-	 phone integer,
-	 PRIMARY KEY(VAT, phone),
-	 FOREIGN KEY(VAT) REFERENCES person(VAT));
+    (VAT INTEGER,
+     phone VARCHAR(15),
+     PRIMARY KEY(VAT, phone),
+     FOREIGN KEY(VAT) REFERENCES person(VAT));
 
 CREATE TABLE client
-	(VAT INTEGER,
-	 PRIMARY KEY(VAT),
-	 FOREIGN KEY(VAT) REFERENCES person(VAT));
+    (VAT INTEGER,
+     PRIMARY KEY(VAT),
+     FOREIGN KEY(VAT) REFERENCES person(VAT));
 
 CREATE TABLE veterinary
-	(VAT INTEGER,
-	 specialization VARCHAR(20),
-	 bio VARCHAR(255),
-	 PRIMARY KEY(VAT),
-	 FOREIGN KEY(VAT) REFERENCES person(VAT));
+    (VAT INTEGER,
+     specialization VARCHAR(20),
+     bio VARCHAR(255),
+     PRIMARY KEY(VAT),
+     FOREIGN KEY(VAT) REFERENCES person(VAT));
 
 CREATE TABLE assistant
-	(VAT INTEGER,
-	 PRIMARY KEY(VAT),
-	 FOREIGN KEY(VAT) REFERENCES person(VAT));
+    (VAT INTEGER,
+     PRIMARY KEY(VAT),
+     FOREIGN KEY(VAT) REFERENCES person(VAT));
 
 CREATE TABLE species
-	(name VARCHAR(35),
-	 descript VARCHAR(100),
-	 PRIMARY KEY(name));
+    (name VARCHAR(35),
+     descript VARCHAR(255),
+     PRIMARY KEY(name));
 
 CREATE TABLE generalization_species
-	(name1 VARCHAR(35),
-	 name2 VARCHAR(35),
-	 PRIMARY KEY(name1),
-	 FOREIGN KEY(name1) REFERENCES species(name),
-	 FOREIGN KEY(name2) REFERENCES species(name));
-	 
+    (name1 VARCHAR(35),
+     name2 VARCHAR(35),
+     PRIMARY KEY(name1),
+     FOREIGN KEY(name1) REFERENCES species(name),
+     FOREIGN KEY(name2) REFERENCES species(name));
+
 CREATE TABLE animal
-	(name VARCHAR(35),
-	 VAT INTEGER,
-	 species_name VARCHAR(35),
-	 colour VARCHAR(15),
-	 gender VARCHAR(15),
-	 birth_year DATE,
-	 age INTEGER,
-	 PRIMARY KEY(name, VAT),
-	 FOREIGN KEY(VAT) REFERENCES client(VAT),
-	 FOREIGN KEY(species_name) REFERENCES species(name));
+    (name VARCHAR(35),
+     VAT INTEGER,
+     species_name VARCHAR(35),
+     colour VARCHAR(15),
+     gender VARCHAR(15),
+     birth_year DATE,
+     age INTEGER,
+     PRIMARY KEY(name, VAT),
+     FOREIGN KEY(VAT) REFERENCES client(VAT),
+     FOREIGN KEY(species_name) REFERENCES species(name));
 
 CREATE TABLE consult
-	(name VARCHAR(35),
-	 VAT_owner INTEGER,
-	 date_timestamp TIMESTAMP,
-	 s VARCHAR(255),
-	 o VARCHAR(255),
-	 a VARCHAR(255),
-	 p VARCHAR(255),
-	 VAT_client INTEGER,
-	 VAT_vet INTEGER,
-	 weight NUMERIC(6,3) NOT NULL, /*COnfimei se é assim por favor!!!*/
-	 PRIMARY KEY(name, VAT_owner, date_timestamp),
-	 FOREIGN KEY(name, VAT_owner) REFERENCES animal(name, VAT),
-	 FOREIGN KEY(VAT_client) REFERENCES client(VAT),
-	 FOREIGN KEY(VAT_vet) REFERENCES veterinary(VAT),
+    (name VARCHAR(35),
+     VAT_owner INTEGER,
+     date_timestamp TIMESTAMP,
+     s VARCHAR(255),
+     o VARCHAR(255),
+     a VARCHAR(255),
+     p VARCHAR(255),
+     VAT_client INTEGER,
+     VAT_vet INTEGER,
+     weight NUMERIC(6,3) NOT NULL, /*COnfimei se é assim por favor!!!*/
+     PRIMARY KEY(name, VAT_owner, date_timestamp),
+     FOREIGN KEY(name, VAT_owner) REFERENCES animal(name, VAT),
+     FOREIGN KEY(VAT_client) REFERENCES client(VAT),
+     FOREIGN KEY(VAT_vet) REFERENCES veterinary(VAT),
      CHECK (weight > 0));
 
 CREATE TABLE participation
-	(name VARCHAR(35),
-	 VAT_owner INTEGER,
-	 date_timestamp TIMESTAMP,
-	 VAT_assistant INTEGER,
-	 PRIMARY KEY(name, VAT_owner, date_timestamp, VAT_assistant),
-	 FOREIGN KEY(name, VAT_owner, date_timestamp) REFERENCES consult(name, VAT_owner, date_timestamp),
-	 FOREIGN KEY(VAT_assistant) references assistant(VAT));
+    (name VARCHAR(35),
+     VAT_owner INTEGER,
+     date_timestamp TIMESTAMP,
+     VAT_assistant INTEGER,
+     PRIMARY KEY(name, VAT_owner, date_timestamp, VAT_assistant),
+     FOREIGN KEY(name, VAT_owner, date_timestamp) REFERENCES consult(name, VAT_owner, date_timestamp),
+     FOREIGN KEY(VAT_assistant) references assistant(VAT));
 
 create table diagnosis_code
-	(code varchar(50),
-	 name varchar(35),
-	 primary key(code));
+    (code varchar(50),
+     name varchar(35),
+     primary key(code));
 
 create table consult_diagnosis
-	(code varchar(50),
-	 name varchar(35),
-	 VAT_owner integer,
-	 date_timestamp timestamp,
-	 primary key(code, name, VAT_owner, date_timestamp),
-	 foreign key(name) references consult(name),
-	 foreign key(VAT_owner) references consult(VAT_owner),
-	 foreign key(date_timestamp) references consult(date_timestamp),
-	 foreign key(code) references diagnosis_code(code));
+    (code varchar(50),
+     name varchar(35),
+     VAT_owner integer,
+     date_timestamp timestamp,
+     primary key(code, name, VAT_owner, date_timestamp),
+     foreign key(name) references consult(name),
+     foreign key(VAT_owner) references consult(VAT_owner),
+     foreign key(date_timestamp) references consult(date_timestamp),
+     foreign key(code) references diagnosis_code(code));
 
 create table medication
-	(name VARCHAR(35),
-	 lab VARCHAR(35),
-	 dosage INTEGER,
-	 PRIMARY KEY(name, lab, dosage));
+    (name VARCHAR(35),
+     lab VARCHAR(35),
+     dosage INTEGER,
+     PRIMARY KEY(name, lab, dosage));
 
 CREATE TABLE prescription 
-	(code VARCHAR(50),
-	 name VARCHAR(35),
-	 VAT_owner INTEGER,
-	 date_timestamp TIMESTAMP,
-	 name_med VARCHAR(35),
-	 lab VARCHAR(35),
-	 dosage INTEGER,
-	 regime VARCHAR(50),
-	 PRIMARY KEY(code, name, VAT_owner, date_timestamp, name_med, lab,dosage),
-	 FOREIGN KEY(code) REFERENCES consult_diagnosis(code),
-	 FOREIGN KEY(name) REFERENCES consult_diagnosis(name),
-	 FOREIGN KEY(VAT_owner) REFERENCES consult_diagnosis(VAT_owner),
-	 FOREIGN KEY(date_timestamp) REFERENCES consult_diagnosis(date_timestamp),
-	 FOREIGN KEY(name_med) REFERENCES medication(name),
-	 FOREIGN KEY(lab) REFERENCES medication(lab),
-	 FOREIGN KEY(dosage) REFERENCES medication(dosage));
+    (code VARCHAR(50),
+     name VARCHAR(35),
+     VAT_owner INTEGER,
+     date_timestamp TIMESTAMP,
+     name_med VARCHAR(35),
+     lab VARCHAR(35),
+     dosage INTEGER,
+     regime VARCHAR(50),
+     PRIMARY KEY(code, name, VAT_owner, date_timestamp, name_med, lab,dosage),
+     FOREIGN KEY(code) REFERENCES consult_diagnosis(code),
+     FOREIGN KEY(name) REFERENCES consult_diagnosis(name),
+     FOREIGN KEY(VAT_owner) REFERENCES consult_diagnosis(VAT_owner),
+     FOREIGN KEY(date_timestamp) REFERENCES consult_diagnosis(date_timestamp),
+     FOREIGN KEY(name_med) REFERENCES medication(name),
+     FOREIGN KEY(lab) REFERENCES medication(lab),
+     FOREIGN KEY(dosage) REFERENCES medication(dosage));
 
 CREATE TABLE indicator
-	(name VARCHAR(35),
-	 reference_value INTEGER,
-	 units VARCHAR(10),
-	 descriptions VARCHAR(255),
-	 PRIMARY KEY(name),
-	);
+    (name VARCHAR(35),
+     reference_value INTEGER,
+     units VARCHAR(10),
+     descriptions VARCHAR(255),
+     PRIMARY KEY(name),
+    );
 
 CREATE TABLE procedures
-	(name VARCHAR(35),
-	 VAT_owner INTEGER,
-	 date_timestamp TIMESTAMP,
-	 num INTEGER,
-	 descriptions VARCHAR(255),
-	 PRIMARY KEY(name, VAT_owner, date_timestamp, num),
-	 FOREIGN KEY(name) REFERENCES consult(name),
-	 FOREIGN KEY(VAT_owner) REFERENCES consult(VAT_owner),
-	 FOREIGN KEY(date_timestamp) REFERENCES consult(date_timestamp));
+    (name VARCHAR(35),
+     VAT_owner INTEGER,
+     date_timestamp TIMESTAMP,
+     num INTEGER,
+     descriptions VARCHAR(255),
+     PRIMARY KEY(name, VAT_owner, date_timestamp, num),
+     FOREIGN KEY(name) REFERENCES consult(name),
+     FOREIGN KEY(VAT_owner) REFERENCES consult(VAT_owner),
+     FOREIGN KEY(date_timestamp) REFERENCES consult(date_timestamp));
 
 CREATE TABLE performed
-	(name VARCHAR(35),
-	 VAT_owner INTEGER,
-	 date_timestamp TIMESTAMP,
-	 num INTEGER,
-	 VAT_assistant INTEGER,
-	 PRIMARY KEY(name, VAT_owner, date_timestamp, num, VAT_assistant),
-	 FOREIGN KEY(name) REFERENCES procedures(name),
-	 FOREIGN KEY(VAT_owner) REFERENCES procedures(VAT_owner),
-	 FOREIGN KEY(date_timestamp) REFERENCES procedures(date_timestamp),
-	 FOREIGN KEY(num) REFERENCES procedures(num),
-	 FOREIGN KEY(VAT_assistant) REFERENCES assistant(VAT));
+    (name VARCHAR(35),
+     VAT_owner INTEGER,
+     date_timestamp TIMESTAMP,
+     num INTEGER,
+     VAT_assistant INTEGER,
+     PRIMARY KEY(name, VAT_owner, date_timestamp, num, VAT_assistant),
+     FOREIGN KEY(name) REFERENCES procedures(name),
+     FOREIGN KEY(VAT_owner) REFERENCES procedures(VAT_owner),
+     FOREIGN KEY(date_timestamp) REFERENCES procedures(date_timestamp),
+     FOREIGN KEY(num) REFERENCES procedures(num),
+     FOREIGN KEY(VAT_assistant) REFERENCES assistant(VAT));
 
 CREATE TABLE radiography
-	(name VARCHAR(35),
-	 VAT_owner INTEGER,
-	 date_timestamp TIMESTAMP,
-	 num INTEGER,
-	 file_path VARCHAR(50),
-	 PRIMARY KEY(name, VAT_owner, date_timestamp, num),
-	 FOREIGN KEY(name) REFERENCES procedures(name),
-	 FOREIGN KEY(VAT_owner) REFERENCES procedures(VAT_owner),
-	 FOREIGN KEY(date_timestamp) REFERENCES procedures(date_timestamp),
-	 FOREIGN KEY(num) REFERENCES procedures(num));
+    (name VARCHAR(35),
+     VAT_owner INTEGER,
+     date_timestamp TIMESTAMP,
+     num INTEGER,
+     file_path VARCHAR(50),
+     PRIMARY KEY(name, VAT_owner, date_timestamp, num),
+     FOREIGN KEY(name) REFERENCES procedures(name),
+     FOREIGN KEY(VAT_owner) REFERENCES procedures(VAT_owner),
+     FOREIGN KEY(date_timestamp) REFERENCES procedures(date_timestamp),
+     FOREIGN KEY(num) REFERENCES procedures(num));
 
 CREATE TABLE test_procedure
-	(name VARCHAR(35),
-	 VAT_owner INTEGER,
-	 date_timestamp TIMESTAMP,
-	 num INTEGER,
-	 test_type VARCHAR(35),
-	 PRIMARY KEY(name, VAT_owner, date_timestamp, num),
-	 FOREIGN KEY(name) REFERENCES procedures(name),
-	 FOREIGN KEY(VAT_owner) REFERENCES procedures(VAT_owner),
-	 FOREIGN KEY(date_timestamp) REFERENCES procedures(date_timestamp),
-	 FOREIGN KEY(num) REFERENCES procedures(num)
-	);
+    (name VARCHAR(35),
+     VAT_owner INTEGER,
+     date_timestamp TIMESTAMP,
+     num INTEGER,
+     test_type VARCHAR(35),
+     PRIMARY KEY(name, VAT_owner, date_timestamp, num),
+     FOREIGN KEY(name) REFERENCES procedures(name),
+     FOREIGN KEY(VAT_owner) REFERENCES procedures(VAT_owner),
+     FOREIGN KEY(date_timestamp) REFERENCES procedures(date_timestamp),
+     FOREIGN KEY(num) REFERENCES procedures(num)
+    );
 
 CREATE TABLE produced_indicator
-	(name VARCHAR(35),
-	 VAT_owner INTEGER,
-	 date_timestamp TIMESTAMP,
-	 num INTEGER,
-	 indicator_name VARCHAR(35),
-	 p_value INTEGER,
-	 PRIMARY KEY(name, VAT_owner, date_timestamp, num, indicator_name),
-	 FOREIGN KEY(name) REFERENCES test_procedure(name),
-	 FOREIGN KEY(VAT_owner) REFERENCES test_procedure(VAT_owner),
-	 FOREIGN KEY(date_timestamp) REFERENCES test_procedure(date_timestamp),
-	 FOREIGN KEY(num) REFERENCES test_procedure(num),
-	 FOREIGN KEY(indicator_name) REFERENCES indicator(name));
+    (name VARCHAR(35),
+     VAT_owner INTEGER,
+     date_timestamp TIMESTAMP,
+     num INTEGER,
+     indicator_name VARCHAR(35),
+     p_value INTEGER,
+     PRIMARY KEY(name, VAT_owner, date_timestamp, num, indicator_name),
+     FOREIGN KEY(name) REFERENCES test_procedure(name),
+     FOREIGN KEY(VAT_owner) REFERENCES test_procedure(VAT_owner),
+     FOREIGN KEY(date_timestamp) REFERENCES test_procedure(date_timestamp),
+     FOREIGN KEY(num) REFERENCES test_procedure(num),
+     FOREIGN KEY(indicator_name) REFERENCES indicator(name));
--- project2/populate.sql
+++ project2/populate.sql
@@ -1,36 +1,49 @@
 /* persons */
 INSERT INTO person
-VALUES (1221, 'John Smith', 'Fifth Street', 'Manhathan', '12311-213');
+VALUES (1221, 'John Smith', '5th Street', 'Manhathan', '12311-213');
 INSERT INTO veterinary
 VALUES (1221, 'surgeon', 'Bacon ipsum dolor amet sausage tongue drumstick, pork chop picanha shankle pork loin capicola corned beef shank porchetta bacon.');
-INSERT INTO client
-VALUES (1221);
+INSERT INTO client VALUES (1221);
+INSERT INTO phone_number VALUES (1221, '312-14124')
+INSERT INTO phone_number VALUES (1221, '131-16514')
 
 INSERT INTO person
-VALUES (9126, 'Maddison Ivy', 'Fifth Street', 'Manhathan', '3123-545');
+VALUES (9126, 'Maddison Livy', '11th Street', 'Manhathan', '12311-301');
 INSERT INTO veterinary
 VALUES (9126, 'anesthesiologist', 'Veggies es bonus vobis, proinde vos postulo essum magis kohlrabi welsh onion daikon amaranth tatsoi tomatillo melon azuki bean garlic.');
+INSERT INTO phone_number VALUES (9126, '412-13131')
 
 INSERT INTO person
 VALUES (5121, 'Ralph Lifshitz', 'The Bronx', 'New York City', '521231-123');
-INSERT INTO assistant
-VALUES (5121)
+INSERT INTO assistant VALUES (5121)
+INSERT INTO phone_number VALUES (5121, '231-81411')
 
 INSERT INTO person
-VALUES (12311, 'Harry Potter', 'Hogwartz, Gryphyndor Hall', '', '');
-INSERT INTO client
-VALUES (12311);
+VALUES (12311, 'Harry Potter', 'Gryffindor Hall', 'Hogwartz', '12314-676');
+INSERT INTO client VALUES (12311);
+INSERT INTO phone_number VALUES (12311, '222-22222')
 
 INSERT INTO person
-VALUES (21321, '', '', '', '');
-
-INSERT INTO client
-VALUES (21321);
+VALUES (21321, 'Neville Longbottom', 'Gryffindor Hall', 'Hogwartz', '12314-676');
+INSERT INTO client VALUES (21321);
+INSERT INTO phone_number VALUES (21321, '111-11111')
 
 /* species */
+INSERT INTO species VALUES('mammal', 'Mammals are vertebrates, distinguished from reptiles (including birds) by the possession of a neocortex (a region of the brain), hair, three middle ear bones, and mammary glands.');
+
+INSERT INTO species VALUES('bird', 'Birds are a group of endothermic vertebrates, characterised by feathers, toothless beaked jaws, the laying of hard-shelled eggs, ...');
+INSERT INTO species VALUES('owl', 'Owls area a species of mostly solitary and nocturnal birds of prey typified by an upright stance, a large, broad head, binocular vision, binaural hearing, sharp talons, and feathers adapted for silent flight. ');
+INSERT INTO generalization_species VALUES ('owl', 'bird');
+
+INSERT INTO species VALUES('amphibian', 'Amphibians are ectothermic, tetrapod vertebrates of the class Amphibia.');
+INSERT INTO species VALUES('frog', 'A frog is any member of a diverse and largely carnivorous group of short-bodied, tailless amphibians composing the order Anura');
+INSERT INTO generalization_species VALUES ('frog', 'amphibian');
 
 /* animals */
 INSERT INTO animal
-VALUES ('Hedwig', 12311, 'owl', 'white', 'female', '1992-12-12');
+VALUES ('Hedwig', 12311, 'owl', 'white', 'female', '2009-12-12', 9);
+
+INSERT INTO animal
+VALUES ('Trevor', 21321, 'frog', 'green', 'male', '1992-12-12', 26);
 
 /* consult */
