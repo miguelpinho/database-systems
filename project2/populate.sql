f5162d1aef69d90cacbd0a2b421539cb27248eae
miguelp <33704675+miguelpinho@users.noreply.github.com>
2018-11-04 20:59:57 +0000

    buffed up database

--- project2/populate.sql
+++ project2/populate.sql
@@ -74,14 +74,38 @@ VALUES (77777, 'John Snow', 'The Wall', 'The North', '67131-974');
 INSERT INTO client VALUES (77777);
 INSERT INTO phone_number VALUES (77777, '412-34212');
 
+INSERT INTO person
+VALUES (51361, 'Gandalf, the White', 'White Tower', 'Middle Earth', '41731-312');
+INSERT INTO client VALUES (51361);
+INSERT INTO phone_number VALUES (51361, '841-31412');
+
+INSERT INTO person
+VALUES (15467, 'Steven White', '1910 Pine Tree Lane', 'Washington', '20005'),
+       (84698, 'Sarah Madrigal', '3097 Hinkle Lake Road', 'Binghamton', '13901'),
+       (63799, 'Philip Fry', '1497 Giraffe Hill Drive', 'Irving', '75060'),
+       (62736, 'Kelly Mueller', '2797 Bird Street', 'Columbus', '88029'),
+       (41461, 'Joseph Mueller', '2797 Bird Street', 'Columbus', '88029'),
+       (70627, 'Enrique Edwards', '2425 Penn Street', 'Columbia', '65202');
+INSERT INTO client VALUES (15467), (84698), (63799), (62736), (70627);
+INSERT INTO phone_number
+VALUES (15467, '(309)218-6902'), (84698, '(914)545-5719'), (63799, '(702)454-7299'), (62736, '(860)359-0003'), (70627, '(254)298-0325');
+
 /* species */
 INSERT INTO species
 VALUES ('mammal', 'Mammals are vertebrates, distinguished from reptiles (including birds) by the possession of a neocortex (a region of the brain), hair, three middle ear bones, and mammary glands.'),
        ('canidae', 'The biological family Canidae is a lineage of carnivorans that includes domestic dogs, wolves, coyotes, foxes, jackals, dingoes, and many other extant and extinct dog-like mammals.'),
+       ('direwolf', 'The dire wolf (Canis dirus, "fearsome dog") is an extinct species of the genus Canis.'),
        ('dog', 'The domestic dog is a member of the genus Canis, which forms part of the wolf-like canids, and is the most widely abundant terrestrial carnivore'),
-       ('direwolf', 'The dire wolf (Canis dirus, "fearsome dog") is an extinct species of the genus Canis.');
+       ('pug', 'The Pug is a breed of dog with physically distinctive features of a wrinkly, short-muzzled face, and curled tail.'),
+       ('golden retriever', 'The Golden Retriever is a large-sized breed of dog bred as gun dogs to retrieve shot waterfowl, such as ducks and upland game birds, during hunting and shooting parties.'),
+       ('german sheppard', 'The German Shepherd is a breed of medium to large-sized working dog that originated in Germany.'),
+       ('beagle', 'The Beagle is a breed of small hound that is similar in appearance to the much larger foxhound.'),
+       ('border terrier', 'The Border Terrier is a small, rough-coated breed of dog in the terrier group.'),
+       ('staffordshire bull terrier', 'The Staffordshire Bull Terrier is a medium-sized, short-coated breed of dog of English lineage and may be considered to be within the pit bull type.');
+
 INSERT INTO generalization_species
-VALUES ('canidae', 'mammal'), ('dog', 'canidae'), ('direwolf', 'canidae');
+VALUES ('canidae', 'mammal'), ('direwolf', 'canidae'), ('dog', 'canidae'),
+       ('pug', 'dog'), ('golden retriever', 'dog'), ('german sheppard', 'dog'), ('beagle', 'dog'), ('border terrier', 'dog'), ('staffordshire bull terrier', 'dog');
 
 INSERT INTO species
 VALUES ('bird', 'Birds are a group of endothermic vertebrates, characterised by feathers, toothless beaked jaws, the laying of hard-shelled eggs, ...'),
@@ -97,21 +121,45 @@ INSERT INTO generalization_species VALUES ('frog', 'amphibian');
 INSERT INTO animal
 VALUES ('Hedwig', 12311, 'owl', 'white', 'female', '2009-04-06', 9),
        ('Trevor', 21321, 'frog', 'green', 'male', '2008-12-12', 9),
+       ('Hawk1', 51361, 'bird', 'white', 'male', '2001-06-10', 17),
+       ('Hawk2', 51361, 'bird', 'white', 'male', '2001-06-11', 17),
+       ('Hawk3', 51361, 'bird', 'white', 'male', '2001-06-12', 17),
        ('Summer', 86137, 'direwolf', 'silverly grey', 'male', '2010-04-10', 8),
        ('Nymeria', 71321, 'direwolf', 'grey', 'female', '2010-04-10', 8),
        ('Ghost', 77777, 'direwolf', 'white', 'male', '2010-04-10', 8);
 
+INSERT INTO animal
+VALUES ('Bailey', 15467, 'pug', 'white', 'male', '2010-12-07', 7),
+       ('Bailey', , 'border terrier', 'black', 'male', '2011-01-13', 7),
+       ('Max', , 'golden sheppard', 'golden', 'male', '2013-02-04', 5),
+       ('Max', 15467, 'pug', 'brown', 'male', '2016-05-26', 2),
+       ('Charlie', , 'beagle', 'white', 'male', '2016-04-08', 2),
+       ('Buddy', , 'golden retriever', 'golden', 'male', '2013-02-26', 5),
+       ('Bella', , 'pug', 'silverly grey', 'female', '2011-06-11', 7),
+       ('Lucy', , 'golden sheppard', 'golden', 'female', '2010-02-25', 8),
+       ('Daisy', , 'beagle', 'yellow', 'female', '2013-02-14', 5),
+       ('Maggie', 15467, 'pug', 'pink', 'female', '2015-09-09', 3),
+       ('Molly', , 'border terrier', 'white', 'female', '2009-09-16
+', 9),
+       ('Molly', , 'german sheppard', 'dark brown', 'female', '2017-09-05', 1);
+
 /* diagnosis code */
 INSERT INTO diagnosis_code
-VALUES ('AHAA 3124', 'kidney failure'),
-       ('AHAA 1832', 'insomnia'),
-       ('AHAA 2141', 'obesity'),
-       ('AHAA 1923', 'stress'),
-       ('AHAA 3100', 'heart problem'),
-       ('AHAA 3110', 'diabetes'),
-       ('AHAA 1412', 'sleepyness'),
-       ('AHAA 0132', 'broken paw'),
-       ('AHAA 5000', 'not aliveness');
+VALUES ('AAHA 3124', 'kidney failure'),
+       ('AAHA 1832', 'insomnia'),
+       ('AAHA 2141', 'obesity'),
+       ('AAHA 1923', 'stress'),
+       ('AAHA 3100', 'acute myocardial infraction'),
+       ('AAHA 5651', 'osteoporosis'),
+       ('AAHA 5314', 'superficial injury'),
+       ('AAHA 3093', 'fracture'),
+       ('AAHA 6731', 'asthma'),
+       ('AAHA 3110', 'diabetes'),
+       ('AAHA 1412', 'sleepyness'),
+       ('AAHA 1913', 'pneumonia'),
+       ('AAHA 0132', 'broken paw'),
+       ('AAHA 8712', 'hypertension'),
+       ('AAHA 5000', 'not aliveness');
 
 /* medication */
 INSERT INTO medication
@@ -165,21 +213,21 @@ VALUES ('Summer', 86137, '2017-03-03 15:00:00', 1010),
 
 /* consult diagnosis */
 INSERT INTO consult_diagnosis
-VALUES ('AHAA 1832', 'Hedwig', 12311, '2017-09-02 10:00:00');
+VALUES ('AAHA 1832', 'Hedwig', 12311, '2017-09-02 10:00:00');
 
 INSERT INTO consult_diagnosis
-VALUES ('AHAA 1412', 'Summer', 86137, '2017-03-03 15:00:00'),
-       ('AHAA 1832', 'Summer', 86137, '2017-04-15 08:00:00'),
-       ('AHAA 1923', 'Summer', 86137, '2017-04-15 08:00:00'),
-       ('AHAA 2141', 'Summer', 86137, '2017-05-31 12:00:00');
+VALUES ('AAHA 1412', 'Summer', 86137, '2017-03-03 15:00:00'),
+       ('AAHA 1832', 'Summer', 86137, '2017-04-15 08:00:00'),
+       ('AAHA 1923', 'Summer', 86137, '2017-04-15 08:00:00'),
+       ('AAHA 2141', 'Summer', 86137, '2017-05-31 12:00:00');
 
 /* presciption */
 INSERT INTO prescription
-VALUES ('AHAA 1832', 'Hedwig', 12311, '2017-09-02 10:00:00', 'Xanax', 'Niravam', 100, 'One pill one hour before bedtime');
+VALUES ('AAHA 1832', 'Hedwig', 12311, '2017-09-02 10:00:00', 'Xanax', 'Niravam', 100, 'One pill one hour before bedtime');
 
 INSERT INTO prescription
-VALUES ('AHAA 1412', 'Summer', 86137, '2017-03-03 15:00:00', 'Calming Tea', 'Lipton', 1, 'Two doses a day'),
-       ('AHAA 1832', 'Summer', 86137, '2017-04-15 08:00:00', 'Xanax', 'Niravam', 100, 'One pill one hour before bedtime');
+VALUES ('AAHA 1412', 'Summer', 86137, '2017-03-03 15:00:00', 'Calming Tea', 'Lipton', 1, 'Two doses a day'),
+       ('AAHA 1832', 'Summer', 86137, '2017-04-15 08:00:00', 'Xanax', 'Niravam', 100, 'One pill one hour before bedtime');
 
 /* procedures */
 INSERT INTO procedures
