eeb952cfd8808d3e1b3a28b7a5fc8e8aa50c78eb
malaca3 <miguelmmalaca@tecnico.ulisboa.pt>
2018-11-05 14:18:03 +0000

    Update project2/changes.sql
    2 changes available

--- project2/changes.sql
+++ project2/changes.sql
@@ -1,8 +1,18 @@
 /* 1. */
-update person set address_street = 'Rua Comandante', address_city = 'Alcanena', address_zip = '2380-048' where name = 'John Smith';
+update person 
+set address_street = 'Rua Comandante', address_city = 'Alcanena', address_zip = '2380-048' 
+	where name = 'John Smith';
+
 /* 2. */
+update indicator 
+	inner join produced_indicator 
+		on indicator.name = produced_indicator.indicator_name 
+	inner join test_procedure 
+		on test_procedure.num = produced_indicator.num 
+set reference_value = 1.1 *  reference_value
+	where test_procedure.test_type = 'Blood analysis' 
+	and indicator.units = 'miligrams';
 
 /* 3. */
 
 /* 4. */
-
