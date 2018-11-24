2b80f96bd9d118e805166c2d45f47c312a53a74c
Miguel Pinho <miguel.m.pinho@tecnico.ulisboa.pt>
2018-11-24 12:21:13 +0000

    add trigger 2)

--- project3/triggers.sql
+++ project3/triggers.sql
@@ -1,3 +1,7 @@
+DROP TRIGGER IF EXISTS animal_age;
+DROP TRIGGER IF EXISTS already_assistant;
+DROP TRIGGER IF EXISTS already_vet;
+
 delimiter $$
 
 /* 1. update animal age */
@@ -9,11 +13,25 @@ BEGIN
     WHERE name = new.name AND VAT = new.VAT_owner;
 END$$
 
-/* 2. ensure vet is not assistant, or vice-versa */
-CREATE TRIGGER already_assistant before INSERT ON veterinary
+/* 2. ensure vet is not already assistant, and vice-versa */
+CREATE TRIGGER already_assistant BEFORE INSERT ON veterinary
 FOR EACH ROW
 BEGIN
+    DECLARE msg VARCHAR(255);
+    IF EXISTS (SELECT assistant.VAT FROM assistant WHERE assistant.VAT = new.VAT) THEN
+        SET msg = "IC: person is already an assistant, cannot be a vet too.";
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = msg;
+    END IF;
+END$$
 
+CREATE TRIGGER already_vet BEFORE INSERT ON assistant
+FOR EACH ROW
+BEGIN
+    DECLARE msg VARCHAR(255);
+    IF EXISTS (SELECT veterinary.VAT FROM veterinary WHERE veterinary.VAT = new.VAT) THEN
+        SET msg = "IC: person is already a vet, cannot be a assistant too.";
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = msg;
+    END IF;
 END$$
 
 
