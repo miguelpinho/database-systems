29cc4f4c39d5f4c4e15f34087a1e9488a3266d5e
Carlos <c.david.pires@tecnico.ulisboa.pt>
2018-11-25 15:57:26 +0000

    Update new_consult.php

--- project3/new_consult.php
+++ project3/new_consult.php
@@ -6,7 +6,30 @@
     $vat_client=$_REQUEST['VAT_client'];
     ?>
     <h1> Add a new consult for <?=$animal_name?></h>
-    <h3>Fill the form</h3>
+    <h3>Fill the form for a new consult</h3>    
+    <form action="add_consult.php" method="post">
+    <p>Veterinary  VAT:
+        <input type="text" name="vet_vat"/>
+    </p>
+    <p>Animal weight:
+        <input type="text" name="weight"/>
+    </p> 
+    <h4>Consult notes</h4>
+    <p>S:
+        <input type="text" name="s"/>
+    </p>
+    <p>O:
+        <input type="text" name="o"/>
+    </p>
+    <p>A:
+        <input type="text" name="a"/>
+    </p>
+    <p>P:
+        <input type="text" name="P"/>
+    </p>
+    <h4>Diagnostic Codes:
+    <p><input type="submit" value="Search"/></p>
+    </form>
     
 </body>
 </html>
