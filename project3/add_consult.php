3c2dad3bf181d9111469f3b7fad7cc4196372508
Carlos <c.david.pires@tecnico.ulisboa.pt>
2018-11-25 19:46:28 +0000

    Update add_consult.php

--- project3/add_consult.php
+++ project3/add_consult.php
@@ -1,10 +1,22 @@
 <html>
 <body>
     <?php
-        $date=$_REQUEST['date'];
-        echo($date);
-        $num_codes = $_REQUEST['num_codes'];
-        echo($num_codes);
+        $date=$_REQUEST['date'];        
+        $animal_name =$_REQUEST['animal_name'];
+        $owner_vat=(integer)$_REQUEST['owner_vat'];
+        $num_codes=(integer)$_REQUEST['num_codes'];
+        $vet_vat=(integer)$_REQUEST['vet_vat'];
+        $weight=(integer)$_REQUEST['weight'];
+        $s=$_REQUEST['s'];
+        $o=$_REQUEST['o'];
+        $a=$_REQUEST['a'];
+        $p=$_REQUEST['p'];
+        
+
+        for ($i = 1; $i <= $num_codes; $i++){
+            echo ($_REQUEST[$i]);
+            echo("\n");
+        }
        
 
     ?>
