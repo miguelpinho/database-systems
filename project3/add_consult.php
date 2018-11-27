ba470ec6328d01b11b16cab9f3839e01979d1cb2
Carlos <c.david.pires@tecnico.ulisboa.pt>
2018-11-27 23:53:49 +0000

    add_consult done

--- project3/add_consult.php
+++ project3/add_consult.php
@@ -4,8 +4,7 @@
          $date=$_REQUEST['date'];        
          $animal_name =$_REQUEST['animal_name'];
          $owner_vat=(integer)$_REQUEST['owner_vat'];
-         $client_vat=(integer)$_REQUEST['client_vat'];
-         $num_codes=(integer)$_REQUEST['num_codes'];
+         $client_vat=(integer)$_REQUEST['client_vat'];         
          $vet_vat=(integer)$_REQUEST['vet_vat'];
          $weight=floatval($_REQUEST['weight']);
          $s=$_REQUEST['s'];
@@ -29,6 +28,43 @@
              exit();
          }
 
+       
+    
+        
+        /*
+        $sql = "INSERT INTO consult (name, VAT_owner, date_timestamp, VAT_client, VAT_vet, weight,s, o, a, p)
+                values ('$animal_name', $owner_vat, '$date', $client_vat, $vet_vat, $weight, '$s', '$o', '$a', '$p' )";
+        $result = $connection->query($sql);
+        if ($result == FALSE)
+        {
+            $info = $connection->errorInfo();
+            echo("<p>Error: {$info[2]}</p>");
+            exit();
+        }
+        */
+        $stmt=$connection->prepare("INSERT INTO consult (name, VAT_owner, date_timestamp, VAT_client, VAT_vet, weight,s, o, a, p)
+                        values (:name, :vat_owner, :date, :client_vat, :vet_vat, :weight, :s, :o, :a, :p )");
+        $stmt->bindParam(':name', $animal_name);
+        $stmt->bindParam(':vat_owner', $owner_vat);
+        $stmt->bindParam(':date', $date);
+        $stmt->bindParam(':client_vat', $client_vat);
+        $stmt->bindParam(':vet_vat', $vet_vat);
+        $stmt->bindParam(':weight', $weight);
+        $stmt->bindParam(':s', $s);
+        $stmt->bindParam(':o', $o);
+        $stmt->bindParam(':a', $a);
+        $stmt->bindParam(':p', $p); 
+
+        $result=$stmt->execute();
+        if ($result == FALSE)
+        {
+            echo("UPDATE_consult");
+            $info = $connection->errorInfo();
+            echo("<p>Error: {$info[2]}</p>");
+            exit();
+        }
+
+
         $sql = "SELECT code FROM diagnosis_code";
         $stmt = $connection->query($sql);
         if ($stmt == FALSE)
@@ -40,7 +76,7 @@
         $codes = $stmt->fetchAll();
                
         
-        
+        /*
         foreach($codes as $row)
         {
             $code = $row['code'];            
@@ -49,40 +85,13 @@
                 echo("<p>$code\n</p>");
             }
             
-        }
-    
-        
-        
-        $sql = "INSERT INTO consult (name, VAT_owner, date_timestamp, VAT_client, VAT_vet, weight,s, o, a, p)
-                values ('$animal_name', $owner_vat, '$date', $client_vat, $vet_vat, $weight, '$s', '$o', '$a', '$p' )";
-        $result = $connection->query($sql);
-        if ($result == FALSE)
-        {
-            $info = $connection->errorInfo();
-            echo("<p>Error: {$info[2]}</p>");
-            exit();
-        }
-        
-        
-        /*
-        $stmt ->execute([
-            'name' => $animal_name,
-            'vat_owner' => $owner_vat,
-            'date_time' => $date,
-            's'=> $s,
-            'o'=> $o,
-            'a'=>$a,
-            'p'=> $p,
-            'vat_client' => $client_vat,
-            'vet_vat' => $vet_vat,
-            'weight' =>$weight,
-        ]);*/
+        }*/
         foreach($codes as $row)
         {
             $code = $row['code'];            
             if($_POST['codes'][$code]== $code)
             {
-                echo("<p>$code\n</p>");
+                
                 $stmt=$connection->prepare("INSERT INTO consult_diagnosis (code, name, VAT_owner, date_timestamp)
                                             values (:code, :name, :vat_owner, :date)");
                 $stmt->bindParam(':code', $code);
@@ -92,6 +101,7 @@
                 $result=$stmt->execute();
                 if ($result == FALSE)
                 {
+                    echo("UPDATE_codes");
                     $info = $connection->errorInfo();
                     echo("<p>Error: {$info[2]}</p>");
                     exit();
@@ -101,6 +111,7 @@
         }
         
         $connection = null;
+        echo("<p>Consult successfully inserted</p>");
         echo("<a style=\" margin-top:20px;\" href=\"animal_consults.php?animal_name=");
         echo($animal_name);
         echo("&owner_vat=");  
--- project3/new_consult.php
+++ project3/new_consult.php
@@ -8,7 +8,7 @@
     <h1> Add a new consult for <?=$animal_name?></h>
     <h2>Please fill the form for a new consult</h2>
 
-    <form id="myForm" action='add_consult.php?animal_name=<?=$animal_name?>&owner_vat=<?=$vat_owner?>&client_vat=<?=$vat_client?>&num_codes=' method="post">
+    <form id="myForm" action='add_consult.php?animal_name=<?=$animal_name?>&owner_vat=<?=$vat_owner?>&client_vat=<?=$vat_client?>' method="post">
         <h4>Identification</h4>
         <p>Veterinary  VAT:
             <select name="vet_vat">
@@ -71,9 +71,9 @@
             <p>P:</p>
             <textarea name="p" rows="3" cols="40"></textarea>
         </p>
-        <div id="myDIV" >
+        <p id="myDIV" >
             <h4 style="margin:5px">Diagnosis Codes</h4>
-            <!-- <select id="myInput" > -->
+            
             <?php
             $host = "db.ist.utl.pt";
             $user = "ist425321";
@@ -111,42 +111,7 @@
             $connection = null;
             ?>
             
-        </div>
-
-        
-    
-        <script>
-        // Create a new list item when clicking on the "Add" button
-        var num_codes=0;
-        // Create a new list item when clicking on the "Add" button
-            function newElement() {
-                var li = document.createElement("li");
-                var input = document.createElement("input");
-                var inputValue = document.getElementById("myInput").value;
-                var t = document.createTextNode(inputValue);
-                li.appendChild(t);
-
-                if (inputValue === '') {
-                    alert("You must write something!");
-                } else {
-                    num_codes++;
-                    input.setAttribute("type", "hidden");
-                    input.setAttribute("value", inputValue);
-                    input.setAttribute("name", num_codes);
-
-                    document.getElementById("mycodes").appendChild(li);
-                    document.getElementById("mycodes").appendChild(input);
-                }
-                document.getElementById("myInput").value = "";
-            };
-            function myFunction(){
-                var x = document.getElementById("myForm").action;
-                var z=x+num_codes;
-                document.getElementById("myForm").action=z;
-            }
-
-        </script>
-
+        </p>
         <p><input onclick="myFunction()" type="submit" value="Submit"/></p>
     </form>
 
