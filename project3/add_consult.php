c4aa56b269d9b2ae575e72888c6e1dbd27a0f51d
Carlos <c.david.pires@tecnico.ulisboa.pt>
2018-11-27 23:21:51 +0000

    Update add_consult.php

--- project3/add_consult.php
+++ project3/add_consult.php
@@ -99,29 +99,7 @@
             }
             
         }
-
-
-
-
-        for($i=1; $i<=$num_codes; $i++)
-        {
-            echo ($_REQUEST[$i]);
-            echo("\n");
-            $stmt=$connection->prepare("INSERT INTO consult_diagnosis (code, name, VAT_owner, date_timestamp)
-                                            values (:code, :name, :vat_owner, :date)");
-            $stmt->bindParam(':code', $_REQUEST[$i]);
-            $stmt->bindParam(':name', $animal_name);
-            $stmt->bindParam(':vat_owner', $owner_vat);
-            $stmt->bindParam(':date', $date);
-            $result=$stmt->execute();
-            if ($result == FALSE)
-            {
-                echo("ahjsk\n");
-                $info = $connection->errorInfo();
-                echo("<p>Error: {$info[2]}</p>");
-                exit();
-            }
-        }
+        
         $connection = null;
         echo("<a style=\" margin-top:20px;\" href=\"animal_consults.php?animal_name=");
         echo($animal_name);
