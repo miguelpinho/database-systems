9e54e80812263d38cd70e17b304b8a9cfe0e6bfa
Miguel Pinho <miguel.m.pinho@tecnico.ulisboa.pt>
2018-12-02 15:27:16 +0000

    refactor code for the report

--- project3/functions.sql
+++ project3/functions.sql
@@ -4,7 +4,8 @@ DROP PROCEDURE IF EXISTS to_centigrams;
 DELIMITER $$
 
 /* 4. calculate consults in a year */
-CREATE FUNCTION total_consults_year(name VARCHAR(35), VAT_owner INTEGER, c_year INTEGER)
+CREATE FUNCTION total_consults_year(name VARCHAR(35),
+    VAT_owner INTEGER, c_year INTEGER)
     RETURNS INTEGER
 BEGIN
     DECLARE c_count INTEGER;
@@ -21,7 +22,9 @@ BEGIN
     UPDATE produced_indicator AS M
     INNER JOIN indicator AS I
     ON M.indicator_name = I.name
-    SET M.p_value = M.p_value / 10.0 , M.name = M.name, M.VAT_owner = M.VAT_owner , M.date_timestamp = M.date_timestamp , M.num = M.num , M.indicator_name = M.indicator_name
+    SET M.p_value = M.p_value / 10.0 , M.name = M.name,
+        M.VAT_owner = M.VAT_owner, M.date_timestamp = M.date_timestamp,
+        M.num = M.num , M.indicator_name = M.indicator_name
     WHERE units = "milligrams";
 
     UPDATE indicator
--- project3/test_triggers.sql
+++ project3/test_triggers.sql
@@ -2,18 +2,21 @@
 INSERT INTO animal
 VALUES ('Joshua', 15467, 'dog', 'yellow', 'male', '2001-03-03', 10);
 
-SELECT age AS old_age FROM animal WHERE name = 'Joshua' AND VAT = 15467;
+SELECT age AS old_age FROM animal
+WHERE name = 'Joshua' AND VAT = 15467;
 
 INSERT INTO consult
-VALUES ('Joshua', 15467, '2017-01-01 10:10:10', 12311, 4581, 17.2, 's', 'o', 'a', 'p');
+VALUES ('Joshua', 15467, '2017-01-01 10:10:10', 12311, 4581,
+        17.2, 's', 'o', 'a', 'p');
 
-SELECT age AS new_age FROM animal WHERE name = 'Joshua' AND VAT = 15467;
+SELECT age AS new_age FROM animal
+WHERE name = 'Joshua' AND VAT = 15467;
 
 
 /* 2. Test if a vet is allowed to be an assitant... */
 INSERT INTO assistant VALUES (1010);
 UPDATE assistant
-SET VAT = 1010
+    SET VAT = 1010
     WHERE VAT = 3366;
 
 SELECT VAT FROM assistant;
@@ -21,7 +24,7 @@ SELECT VAT FROM assistant;
 /* ... and vice-versa */
 INSERT INTO veterinary VALUES (5121, 'orthopedist', 'description');
 UPDATE veterinary
-SET VAT = 5121, specialization = 'orthopedist', bio = 'description'
+    SET VAT = 5121, specialization = 'orthopedist', bio = '...'
     WHERE VAT = 1221;
 
 SELECT VAT FROM veterinary;
@@ -31,7 +34,7 @@ SELECT VAT FROM veterinary;
 INSERT INTO phone_number VALUES (5121, '741-51251');
 INSERT INTO phone_number VALUES (5121, '312-21189');
 UPDATE phone_number
-SET phone = '741-51251'
+    SET phone = '741-51251'
     WHERE VAT = 77777 and phone = "412-34212";
 
 SELECT * FROM phone_number WHERE VAT = 5121;
