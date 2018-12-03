7ef5439677aafbcd36c594f612c6c7a20531536d
malaca3 <miguelmmalaca@tecnico.ulisboa.pt>
2018-12-03 11:03:59 +0000

    Update add_bt_form.php
    Selection of VAT_assistant

--- project3/add_bt_form.php
+++ project3/add_bt_form.php
@@ -9,7 +9,42 @@
     
     <form action="add_procedure.php" method="post">
         <p>Assistant VAT:
-            <input type="text" name="vat_assistant"/>
+            <select name="vat_assistant">
+            <?php
+            $host = "db.ist.utl.pt";
+            $user = "ist181702";
+            $pass = "nqit9741";
+            $dsn = "mysql:host=$host;dbname=$user";
+            try
+            {
+                $connection = new PDO($dsn, $user, $pass);
+            }
+            catch(PDOException $exception)
+            {
+                echo("<p>Error: ");
+                echo($exception->getMessage());
+                echo("</p>");
+                exit();
+            }
+
+            $sql = "SELECT VAT FROM assistant";
+            $result = $connection->query($sql);
+            if ($result == FALSE)
+            {
+                $info = $connection->errorInfo();
+                echo("<p>Error: {$info[2]}</p>");
+                exit();
+            }
+
+            foreach($result as $row)
+            {
+                $vat = $row['VAT'];
+                echo("<option value=\"$vat\">$vat</option>");
+            }
+
+            $connection = null;
+            ?>
+            </select>
         </p>
         <p>Description:
             <input type="text" name="desc"/>
